5d3e8c72b859410d38f6072db2b9b50297ec174a
Chenzhi Yu <cy2583@columbia.edu>
2021-11-01 01:02:35 +0000

    new transaction in progress

--- groupSplit/config/environments/development.rb
+++ groupSplit/config/environments/development.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  Rails.application.configure do
+    config.hosts.clear
+  end
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
