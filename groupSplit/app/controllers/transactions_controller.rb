185cf9ec3f7f02f6bad389b63a77b97abaef369e
Chenzhi Yu <cy2583@columbia.edu>
2021-11-01 02:08:16 +0000

    TODO: add test cases

--- groupSplit/app/controllers/transactions_controller.rb
+++ groupSplit/app/controllers/transactions_controller.rb
@@ -1,4 +1,22 @@
 class TransactionsController < ApplicationController
+  
+  def index
+    @transactions = Transaction.all
+  end
+    
   def new
   end
+    
+  def create
+    @transaction = Transaction.create!(transaction_params)
+    flash[:notice] = "transaction of #{@transaction.trans_number} was successfully created."
+  end
+    
+  private
+  # Making "internal" methods private is not required, but is a common practice.
+  # This helps make clear which methods respond to requests, and which ones do not.
+  def transaction_params
+    params.require(:transaction).permit(:user_name, :group_name, :trans_number, :trans_type, :group_member, :trans_date)
+  end
+    
 end
