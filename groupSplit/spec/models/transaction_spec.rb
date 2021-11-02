3f22d13dd67d43ce3e1d4ca39ebc2fe125c75d6e
liqin-personal <graves_zhang@sjtu.edu.cn>
2021-11-01 21:00:14 -0400

    add rspec for transaction model

--- groupSplit/spec/models/transaction_spec.rb
+++ groupSplit/spec/models/transaction_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
-RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Transaction, :type => :model do
+  it "is valid with valid attributes" do
+    t = Transaction.new
+    t.user_name = "testuser"
+    t.group_name = "testgroup"
+    t.trans_number = 1000
+    t.trans_type = "Food"
+    t.group_member = "unknown"
+    t.trans_date = DateTime.now
+    expect(t).to be_valid
+  end
+
+  it "is not valid without a user_name" do
+    t = Transaction.new(user_name: nil)
+    expect(t).to_not be_valid
+  end
+
+  it "is not valid without a group_name" do
+    t = Transaction.new(group_name: nil)
+    expect(t).to_not be_valid
+  end
+
+  it "is not valid without a trans_number" do
+    t = Transaction.new(trans_number: nil)
+    expect(t).to_not be_valid
+  end
+
+  it "is not valid without a trans_type" do
+    t = Transaction.new(trans_type: nil)
+    expect(t).to_not be_valid
+  end
+
+  it "is not valid without a group_member" do
+    t = Transaction.new(group_member: nil)
+    expect(t).to_not be_valid
+  end
+
+  it "is not valid without a trans_date" do
+    t = Transaction.new(trans_date: nil)
+    expect(t).to_not be_valid
+  end
 end
