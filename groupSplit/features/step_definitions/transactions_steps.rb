5ee2603bd1f1df9f665c651c7fda413b939bffb9
Liqin (Lukas) Zhang <graves_zhang@sjtu.edu.cn>
2021-11-02 17:50:23 -0400

    Update transactions_steps.rb

--- groupSplit/features/step_definitions/transactions_steps.rb
+++ groupSplit/features/step_definitions/transactions_steps.rb
@@ -11,12 +11,6 @@ Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   expect(page.body.index(e1) < page.body.index(e2))
 end
 
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  rating_list.split(', ').each do |rating|
-    step %{I #{uncheck.nil? ? '' : 'un'}check "ratings_#{rating}"}
-  end
-end
-
 Then /I should see all the transactions/ do
   # Make sure that all the movies in the app are visible in the table
   Transaction.all.each do |transaction|
