c2cbb7ea6bd41559b1a00b94371ca2b9cc29efce
Chenzhi Yu <cy2583@columbia.edu>
2021-11-14 22:27:47 +0000

    clean up

--- groupSplit/app/models/group.rb
+++
@@ -1,6 +0,0 @@
-class Group < ApplicationRecord
-  validates_presence_of :user_name
-  validates_presence_of :group_name
-  validates_presence_of :group_member
-  validates_presence_of :group_date
-end
