35bea2e407f987f0e5e019c6de6e9d60618706f7
Chenzhi Yu <cy2583@columbia.edu>
2021-11-16 05:01:12 +0000

    transaction edit and destroy added

--- groupSplit/app/controllers/members_controller.rb
+++ groupSplit/app/controllers/members_controller.rb
@@ -2,24 +2,24 @@ class MembersController < ApplicationController
   def index
   end
   def new
-      
+    @todo ="pending"
   end
     
   def create
 
     @member = GroupMember.create!(member_params)
+    @member.update({group_id: "pending"})
     flash[:notice] = "Member #{member_params} was successfully added."
     redirect_to '/groupviews/index'
   end
   
+
   
   private
   # Making "internal" methods private is not required, but is a common practice.
   # This helps make clear which methods respond to requests, and which ones do not.
   def member_params
-    if params[:group_id].empty?
-        params[:group_id]="TODO:placeHolder"
-    end
+    params[:group_id]="pending"
     params.require(:group_member).permit(:group_id, :member_name)
   end 
 end
--- groupSplit/app/controllers/transactions_controller.rb
+++ groupSplit/app/controllers/transactions_controller.rb
@@ -1,5 +1,11 @@
 class TransactionsController < ApplicationController
-  
+    
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @transaction = Transaction.find(id) # look up movie by unique ID
+    # will render app/views/transaction/show.<extension> by default
+  end
+    
   def index
   end
     
@@ -14,7 +20,26 @@ class TransactionsController < ApplicationController
     redirect_to '/groupviews/index'
   end
   
+  def destroy
+    @transaction = Transaction.find(params[:id])
+    @transaction.destroy
+    flash[:notice] = "Transaction '#{@transaction.id}' deleted."
+    redirect_to '/groupviews/index'
+  end
   
+    
+  def edit
+    @transaction = Transaction.find(params[:id])
+    @member_names = GroupMember.pluck(:member_name)
+  end
+  
+  def update
+    @transaction = Transaction.find(params[:id])
+    @transaction.update_attributes!(transaction_params)
+    flash[:notice] = "#{@transaction.id} was successfully updated."
+    redirect_to '/groupviews/index'
+  end
+    
   private
   # Making "internal" methods private is not required, but is a common practice.
   # This helps make clear which methods respond to requests, and which ones do not.
