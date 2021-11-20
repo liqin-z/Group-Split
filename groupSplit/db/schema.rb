82083cb86b49f4d71aba581060e48b7c08fc554c
zagbapu <69334935+zagbapu@users.noreply.github.com>
2021-11-20 12:34:42 -0500

    Added logins

--- groupSplit/db/schema.rb
+++ groupSplit/db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_020414) do
+ActiveRecord::Schema.define(version: 2021_11_20_161954) do
 
   create_table "group_members", force: :cascade do |t|
     t.string "group_id"
     t.string "member_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -36,6 +43,18 @@ ActiveRecord::Schema.define(version: 2021_11_15_020414) do
     t.datetime "trans_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
