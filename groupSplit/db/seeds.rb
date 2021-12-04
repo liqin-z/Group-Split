50b1049747c5c09ac68a3e15b45a796be32582a0
Liqin (Lukas) Zhang <graves_zhang@sjtu.edu.cn>
2021-12-04 15:27:37 -0500

    remove unused db seed

--- groupSplit/db/seeds.rb
+++ groupSplit/db/seeds.rb
@@ -10,53 +10,53 @@ Transaction.destroy_all
 # reset id -> work around: use rake db:drop && rake db:create && rake db:migrate
 # ActiveRecord::Base.connection.reset_pk_sequence!(Transaction)
 
-Transaction.create!([{
-    user_name: 'testuser1',
-    group_name: 'testgroup',
-    trans_number: 20,
-    trans_type: "Food",
-    group_member: "unknown",
-    trans_date: "2021-10-30 01:00:00"
-},
-{
-    user_name: 'testuser2',
-    group_name: 'testgroup',
-    trans_number: 40,
-    trans_type: "Transportation",
-    group_member: "unknown",
-    trans_date: "2021-10-30 01:00:00"
-},
-{
-    user_name: 'testuser3',
-    group_name: 'testgroup',
-    trans_number: 100,
-    trans_type: "Entertainment",
-    group_member: "unknown",
-    trans_date: "2021-10-30 01:00:00"
-},
-{
-     user_name: 'testuser2',
-     group_name: 'testgroup',
-     trans_number: 200,
-     trans_type: "Entertainment",
-     group_member: "unknown",
-     trans_date: "2021-10-30 01:00:00"
-},
-{
-    user_name: 'testuser1',
-    group_name: 'testgroup',
-    trans_number: 400,
-    trans_type: "Transportation",
-    group_member: "unknown",
-    trans_date: "2021-10-30 01:00:00"
-},
-{
-    user_name: 'testuser3',
-    group_name: 'testgroup',
-    trans_number: 1000,
-    trans_type: "Food",
-    group_member: "unknown",
-    trans_date: "2021-10-30 01:00:00"
-}])
+# Transaction.create!([{
+#     user_name: 'testuser1',
+#     group_name: 'testgroup',
+#     trans_number: 20,
+#     trans_type: "Food",
+#     group_member: "unknown",
+#     trans_date: "2021-10-30 01:00:00"
+# },
+# {
+#     user_name: 'testuser2',
+#     group_name: 'testgroup',
+#     trans_number: 40,
+#     trans_type: "Transportation",
+#     group_member: "unknown",
+#     trans_date: "2021-10-30 01:00:00"
+# },
+# {
+#     user_name: 'testuser3',
+#     group_name: 'testgroup',
+#     trans_number: 100,
+#     trans_type: "Entertainment",
+#     group_member: "unknown",
+#     trans_date: "2021-10-30 01:00:00"
+# },
+# {
+#      user_name: 'testuser2',
+#      group_name: 'testgroup',
+#      trans_number: 200,
+#      trans_type: "Entertainment",
+#      group_member: "unknown",
+#      trans_date: "2021-10-30 01:00:00"
+# },
+# {
+#     user_name: 'testuser1',
+#     group_name: 'testgroup',
+#     trans_number: 400,
+#     trans_type: "Transportation",
+#     group_member: "unknown",
+#     trans_date: "2021-10-30 01:00:00"
+# },
+# {
+#     user_name: 'testuser3',
+#     group_name: 'testgroup',
+#     trans_number: 1000,
+#     trans_type: "Food",
+#     group_member: "unknown",
+#     trans_date: "2021-10-30 01:00:00"
+# }])
 
-p "Created #{Transaction.count} transactions"
+# p "Created #{Transaction.count} transactions"
